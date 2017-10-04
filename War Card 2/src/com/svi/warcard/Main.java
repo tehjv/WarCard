cda2f2e271dcceacf370cfc34ebebd9283896a17
jcoronel <jcoronel@ARISTAEUS.svi.com.ph>
2017-10-04 08:09:19 +0800

    updated Main

--- War Card 2/src/com/svi/warcard/Main.java
+++ War Card 2/src/com/svi/warcard/Main.java
@@ -2,6 +2,7 @@ package com.svi.warcard;
 
 public class Main {
 	public static void main(String[] args) {
+		//create instance of game and start application
 		Game game = new Game();
 		game.start();
 	}
